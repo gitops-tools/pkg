d69d4ffc6f870878d934f8c6653e918419cddb30
Kevin McDermott <bigkevmcd@gmail.com>
2022-01-25 17:12:41 +0000

    Improve random name generation.
    This limits the name to a controllable number.
    It will also carry over a final - character on the prefix to make it
    clearer where the prefix and name end.

--- names/generator.go
+++ names/generator.go
@@ -5,25 +5,47 @@ import (
 	"math/rand"
 )
 
+const (
+	branchMaxLength  = 100
+	defaultSuffixLen = 5
+)
+
 // RandomGenerator generates a random name prefix.
 type RandomGenerator struct {
-	rand *rand.Rand
+	rand      *rand.Rand
+	MaxLength int
+	SuffixLen int
 }
 
 // New creates and returns a RandomGenerator.
 func New(r *rand.Rand) *RandomGenerator {
-	return &RandomGenerator{rand: r}
+	return &RandomGenerator{rand: r, MaxLength: branchMaxLength, SuffixLen: defaultSuffixLen}
 }
 
-// PrefixedName generates a name from the prefix with an additional 5 random
-// alphabetic characters.
+// PrefixedName generates a name from the prefix with an additional random set
+// of alphabetic characters.
+//
+// If the prefix + the length of the random set of characters would exceed the
+// MaxLength, then the prefix will be trimmed to accomodate the random string.
+//
+// If the prefix ends with "-" this will be preserved in the trimmed string,
+// before adding the prefix.
 // TODO: this should limit the length based on the prefix because branch names
 // have a limit.
 func (g RandomGenerator) PrefixedName(prefix string) string {
 	charset := "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	b := make([]byte, 5)
+	b := make([]byte, g.SuffixLen)
 	for i := range b {
 		b[i] = charset[g.rand.Intn(len(charset))]
 	}
-	return fmt.Sprintf("%s%s", prefix, b)
+	lastChar := ""
+	if len(prefix)+g.SuffixLen > g.MaxLength {
+		trimPoint := g.MaxLength - g.SuffixLen
+		if lc := prefix[len(prefix)-1:]; lc == "-" {
+			lastChar = lc
+			trimPoint -= 1
+		}
+		prefix = prefix[:trimPoint]
+	}
+	return fmt.Sprintf("%s%s%s", prefix, lastChar, b)
 }
--- names/generator_test.go
+++ names/generator_test.go
@@ -6,11 +6,24 @@ import (
 )
 
 func TestGenerator(t *testing.T) {
-	g := RandomGenerator{rand: rand.New(rand.NewSource(100))}
+	g := New(rand.New(rand.NewSource(100)))
 
 	name := g.PrefixedName("testing-")
 
 	if name != "testing-DlPsU" {
 		t.Fatalf("got %v, want %v", name, "testing-DlPsU")
+	}
+}
+
+func TestGenerator_trims_prefix(t *testing.T) {
+	testString := "this-is-a-long-string-"
+	g := New(rand.New(rand.NewSource(100)))
+	g.MaxLength = len(testString)
+
+	name := g.PrefixedName(testString)
+
+	want := "this-is-a-long-s-DlPsU"
+	if name != want {
+		t.Fatalf("got %s want %s", name, want)
 	}
 }
